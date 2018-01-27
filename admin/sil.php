c4a664c4c5002451e87e8648658062b5a6f43d11
Enes Yıldız <enes.yildiz.95@gmail.com>
2018-01-27 14:13:25 +0300

    sil.php
    resimler de silindi

--- admin/sil.php
+++ admin/sil.php
@@ -6,6 +6,22 @@
 <?php 
 include "../connect.php";
 if ($_GET)
+$query = $baglanti->query("SELECT * FROM movies where id=".(int)$_GET['id']); 
+
+		if ( $query->rowCount() )
+		{
+     		foreach( $query as $row )
+			{
+$img=$row['imageurl'];
+	$img="../".$img;
+	if(is_file($img)){
+		unlink($img);
+	}
+	else{
+		echo "resim bulunamadı";
+	}		
+			}
+			}
 {
 if ($baglanti->query("DELETE FROM movies WHERE id =".(int)$_GET['id'])) // id'si seçilen veriyi silme sorgumuzu yazıyoruz.
 {
